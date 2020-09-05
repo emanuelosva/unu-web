37269f29fe9b9472bbfb1595c8206bddd2e519e0
emanuelosva <emanuelosva@gmail.com>
2020-09-05 13:51:38 -0500

    Dashboard metrics

---
+++ src/components/DashboardMetrics.svelte
@@ -0,0 +1,50 @@
+<script>
+  import MetricCard from "./MetricCard.svelte";
+
+  export let events;
+  export let organizations;
+  export let collaborations;
+
+  let participantsInDb = 500;
+</script>
+
+<style>
+  i {
+    font-size: 2em;
+  }
+</style>
+
+<div class="DashboardMetrics">
+  <div class="container-flow">
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <div class="row">
+          <div class="col-6">
+            <MetricCard quantity={events} text="Mis eventos">
+              <i class="fas fa-calendar-alt" />
+            </MetricCard>
+          </div>
+          <div class="col-6">
+            <MetricCard quantity={organizations} text="Mis organizaciones">
+              <i class="fa fa-building-o" aria-hidden="true" />
+            </MetricCard>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <class class="row">
+          <div class="col-6">
+            <MetricCard quantity={collaborations} text="Colaboraciones">
+              <i class="fa fa-handshake-o" aria-hidden="true" />
+            </MetricCard>
+          </div>
+          <div class="col-6">
+            <MetricCard quantity={participantsInDb} text="Registrados">
+              <i class="fas fa-user-friends" />
+            </MetricCard>
+          </div>
+        </class>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/components/MetricCard.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let quantity;
+  export let text;
+</script>
+
+<style>
+  .MetricCard {
+    box-shadow: -1px 1px 5px 0px rgba(0, 0, 0, 0.75);
+    background-color: white;
+  }
+  .Card {
+    box-sizing: border-box;
+    min-height: 180px;
+    margin: 0.7em;
+    padding: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+  }
+  .Card-quantity {
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+</style>
+
+<div class="MetricCard">
+  <div class="Card">
+    <slot />
+    <p class="Card-quantity">{quantity}</p>
+    <p class="Card-text">{text}</p>
+  </div>
+</div>
