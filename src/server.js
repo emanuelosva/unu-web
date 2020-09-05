e393d57805828a44012438e0fbff2a1c9770aa62
emanuelosva <emanuelosva@gmail.com>
2020-09-05 11:30:22 -0500

    Add proxy auth routes and session

--- src/server.js
+++ src/server.js
@@ -3,30 +3,104 @@
  */
 
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
+import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+
+import axios from 'axios';
+import config from './config';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 // Server instance
-const server = polka()
+const server = express()
+
+// Cockie manage
+server.use(cookieParser())
+
+// Body parsers
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 // Static server
 server.use(compression({ threshold: 0 }))
 server.use(sirv('static', { dev }))
 
 
-// Sapper middleware
-server.use(sapper.middleware({
-  session: (req, res) => ({
-    user: req.user,
-  })
-}));
+/**
+ * Auth proxi API
+ */
+const base = config.api.base;
+
+// Login 
+server.post('/api/login', async (req, res) => {
+  try {
+    const { data, status } = await axios({
+      url: `${base}/users/login`,
+      method: 'POST',
+      data: { ...req.body }
+    });
+    if (status === 200) {
+      res.cookie('token', data.accessToken, {
+        maxAge: 7200 * 1000,
+        httpOnly: !dev,
+        secure: !dev,
+      });
+
+      res.status(200).json(data)
+    }
+  } catch (error) {
+    res.status(error.response.status).json({})
+  }
+});
+
+// Signup
+server.post('/api/signup', async (req, res) => {
+  try {
+    const { data, status } = await axios({
+      url: `${base}/users/signup`,
+      method: 'POST',
+      data: { ...req.body }
+    });
+    if (status === 201) {
+      res.cookie('token', data.accessToken, {
+        maxAge: 7200 * 1000,
+        httpOnly: !dev,
+        secure: !dev,
+      });
+      res.status(201).json(data)
+    }
+  } catch (error) {
+    res.status(error.response.status).json({})
+  }
+});
+
+// Logout
+server.post('/api/logout', (req, res) => {
+  req.cookies('token', None, { expires: Date.now(0) })
+  res.end();
+});
 
 
-// Lauch server
+/**
+ * Sapper Middleware and session coockie.
+ */
+
+server.use((req, res, next) => {
+  const { token } = req.cookies;
+  return sapper.middleware({
+    session: () => {
+      return { user: token, token }
+    }
+  })(req, res, next);
+});
+
+
+/**
+ * Launch server
+ */
 server.listen(PORT, err => {
   if (err) throw new Error('Server Error')
   console.log(`Server listening on port: ${PORT}`)
