fa268ef90ff5882f0504a3d21bd139463ebfb9bb
emanuelosva <emanuelosva@gmail.com>
2020-09-08 20:09:35 -0500

    Form card container

---
+++ src/components/FormCard.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<div class="FormCard">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-8 mx-auto">
+        <slot />
+      </div>
+    </div>
+  </div>
+</div>
